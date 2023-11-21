f0a7d0609a22ce88c36df8a890869dc05325409a
Nguyễn Bá Thiêm <104337976+thiemcun203@users.noreply.github.com>
2023-11-22 00:29:45 +0700

    update test

--- test.py
+++ test.py
@@ -33,6 +33,7 @@ with open('data/tasks.csv', 'r') as file:
         row['task_duration'] = int(row['task_duration'])  # Convert duration to integer
         row['task_predecessor'] = ast.literal_eval(row['task_predecessor'])  # Convert predecessors to list
         row['task_skills_req'] = parse_skills(row['task_skills_req'])  # Convert skill requirements to dictionary
+        row['task_coms_req'] = parse_skills(row['task_coms_req'])  # Convert computer requirements to dictionary
         tasks.append(row)
 
 # Continue with the rest of your model setup and constraint definitions
@@ -63,11 +64,6 @@ for t in range(num_tasks):
         y[t, c] = model.NewIntVar(0, computers[c]['quantity'], f'y[{t},{c}]')
 
 
-# Constraints
-# Add predecessor constraints, resource constraints, etc.
-# Assuming 'task_duration' is a field in your 'tasks' that gives the duration of each task
-
-# Create variables for start and finish times of each task
 task_start = {}
 task_finish = {}
 for t in range(num_tasks):
@@ -96,15 +92,17 @@ for time in range(D):
     for c in range(num_computers):
         model.Add(sum(y_x_product[t, c, time] for t in range(num_tasks)) <= computers[c]['quantity'])
 
+for t in range(num_tasks):
+    for type in range(num_computers):
+        CR = tasks[t]['task_coms_req']        
+        # Add constraint for the required number of computers
+        model.Add(sum(y[t,c] for c in range(type,num_computers)) >= sum([CR[c] for c in range(type,num_computers)])  )
 
 
 # Auxiliary variables for the product of r[s, t] and x[t, time]
-# Correct initialization of r variables
 for s in range(num_staff):
     for t in range(num_tasks):
         r[s, t] = model.NewBoolVar(f'r[{s},{t}]')
-
-# Rest of your model setup
 
 # Create r_x_product variables after r variables are initialized
 r_x_product = {}
@@ -116,26 +114,21 @@ for s in range(num_staff):
             if (s, t) in r and (t, time) in x:
                 model.AddMultiplicationEquality(r_x_product[s, t, time], [r[s, t], x[t, time]])
 
-# Rest of your constraints and objective function
-
 
 # Add constraint for staff assignment
 for s in range(num_staff):
     for time in range(D):
         model.Add(sum(r_x_product[s, t, time] for t in range(num_tasks)) <= 1)
 
-
-for t in range(num_tasks):
     for skill, req_level in tasks[t]['task_skills_req'].items():
         for k in range(num_staff):
             # Checking if staff k's skill level is greater or equal to the requirement
             model.Add(r[k, t] * staff[k]['skill_set'].get(skill, 0) >= r[k, t] * req_level)
-
-
+            
+            # model.Add(sum(r[k, t] * (staff[k]['skill_set'].get(skill, 0) >= req_level) for k in range(num_staff)) >= 1)
+    
 for t in range(num_tasks):
-    for skill, req_level in tasks[t]['task_skills_req'].items():
-        model.Add(sum(r[k, t] * (staff[k]['skill_set'].get(skill, 0) >= req_level) for k in range(num_staff)) >= 1)
-
+    model.Add(sum(r[k, t] for k in range(num_staff)) == 1)
 
 # Objective function
 # Minimum project duration variable
@@ -144,35 +137,6 @@ min_project_duration = model.NewIntVar(0, D, 'min_project_duration')
 # Set the minimum project duration as the latest task finish time
 for t in range(num_tasks):
     model.Add(min_project_duration >= task_finish[t])
-    
-# Create a new variable for the result of min_project_duration / 720
-# duration_in_months = model.NewIntVar(0, D//720+2, 'duration_in_months')
-# model.AddDivisionEquality(duration_in_months, min_project_duration, 720)
-
-# salary_factor = 100  # Factor to convert salary to an integer value
-
-# # Convert salaries to integer values and create variables
-# int_salaries = [model.NewIntVar(0, int(sal * salary_factor), f'int_salary_{s}') for s, sal in enumerate([staff_member['salary'] for staff_member in staff])]
-# for s in range(num_staff):
-#     model.Add(int_salaries[s] == int(staff[s]['salary'] * salary_factor))
-
-# # Determine a reasonable upper bound for the salary per month
-# min_salary = min([staff_member['salary'] for staff_member in staff]) * salary_factor
-# upper_bound_salary_per_month = int(min_salary * (D // 720 + 2))
-
-# # Adjust w[s] calculation
-# for s in range(num_staff):
-#     salary_per_month = model.NewIntVar(0, upper_bound_salary_per_month, f'salary_per_month_{s}')
-#     model.AddMultiplicationEquality(salary_per_month, [duration_in_months, int_salaries[s]])
-#     w[s] = salary_per_month
-
-# Rest of your model setup and constraints
-
-
-# Calculating total cost (wages + idle time cost)
-# total_cost = model.NewIntVar(0, 1000000, 'total_cost')
-# model.Add(total_cost == sum(w[s] for s in range(num_staff)))
-
 
 # Combining objectives - prioritize minimizing duration, then cost, then idle time
 w1, w2 = 10, 1# Example weights
@@ -193,3 +157,28 @@ if status == cp_model.OPTIMAL or status == cp_model.FEASIBLE:
     # Print other details like task scheduling, resource allocation, etc.
 else:
     print('No solution found.')
+
+
+for t in range(len(task_start)):
+    print(solver.Value(task_start[t]), end=" ")
+print()
+for t in range(len(task_start)):
+    print(solver.Value(task_finish[t]), end=" ")
+
+print()
+print("this is the task assignment")
+
+for t in range(num_tasks):
+    for s in range(num_staff):
+        if (solver.Value(r[s, t]) == 1):
+            print(f"the task{t} is being done by {s}")
+print()
+
+print("this is the computer used by each task")
+
+for t in range(num_tasks):
+    print(f"Task {t} computer usage:")
+    for c in range(num_computers):
+        print(f"Com_type{c}:",end=" ")
+        print(solver.Value(y[t,c]))
+
